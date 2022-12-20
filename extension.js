0fe52a68e48e459492cae4646c284008c35101ed
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2022-12-20 16:40:26 +0100

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,45 @@
+const ExtensionUtils = imports.misc.extensionUtils;
+const Main = imports.ui.main;
+const { Gio, UPowerGlib:UPower } = imports.gi;
+
+let settings;
+let touchpadWatcherOn, touchpadWatcherOff;
+
+const switchOSK = () => {
+    let status = settings.get_string("send-events")
+    log(status);
+    let value = status == "enabled" ? "false" : "true"; 
+    log(value);
+    try {
+        Gio.Subprocess.new(
+            ["gsettings", "set", "org.gnome.desktop.a11y.applications", "screen-keyboard-enabled", value],
+            Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+        );
+    } catch (e) {
+        logError(e);
+    }
+}
+
+class Extension {
+    constructor() {
+    }
+
+    enable() {
+        settings = ExtensionUtils.getSettings(
+            "org.gnome.desktop.peripherals.touchpad"
+        );
+        touchpadWatcher = settings.connect(
+            "changed::send-events",
+            switchOSK
+        )
+    }
+
+    disable() {
+        settings.disconnect(touchpadWatcher);
+        settings = null;
+    }
+}
+
+function init() {
+    return new Extension();
+}
